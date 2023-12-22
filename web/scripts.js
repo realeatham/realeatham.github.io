ae3ca219b1ccfbd06062265cd78aadb2e82866ef
realeatham <136866675+realeatham@users.noreply.github.com>
2023-12-22 16:39:59 +1100

    temp converter

--- web/scripts.js
+++ web/scripts.js
@@ -1,10 +1,27 @@
+// page theme functions
 function dark(){
     document.getElementById('style').href='/style-dark.css';
     console.log("Switched to dark mode");
     return
-}
+};
 function light(){
     document.getElementById('style').href='/style.css';
     console.log("Switched to light mode");
     return
+};
+
+//temp converter functions
+tempConvert(){
+    const tempUnit = document.getElementById('tempUnit');
+    const tempUnitWanted = document.getElementById('tempUnitWanted');
+    const temp = document.getElementById('temp');
+    let newtemp
+    switch(tempUnit){
+        case tempUnit == tempUnitWanted:
+            newtemp = temp;
+            break;
+        case tempUnit = 'celsius', tempUnitWanted = 'fahrenheit':
+            newtemp = temp * (5/9) + 32
+            break;
+    }
 }
