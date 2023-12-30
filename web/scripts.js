2c0c4be010717a8b76e07d6780b859133a5aeba0
realeatham <136866675+realeatham@users.noreply.github.com>
2023-12-30 22:31:55 +1100

    temp 8

--- web/scripts.js
+++ web/scripts.js
@@ -21,21 +21,21 @@ function tempConvert(){
     //i know this can be done with switch. i cannot work it out, if you know how to actually use it please help
     if (tempUnit == 'fahrenheit'&&  tempUnitWanted == 'celsius'){
         newTemp = (5/9) * (temp-32);
-    };
+    }
     else if (tempUnit == 'celsius'&&  tempUnitWanted == 'fahrenheit'){
         newTemp = (temp * 9/5) + 32;
-    };
+    }
     else if (tempUnit == 'celsius'&&  tempUnitWanted == 'kelvin'){
         newTemp = temp + 273.15;
-    };
+    }
     else if (tempUnit == 'kelvin'&&  tempUnitWanted == 'celsius'){
         newTemp = temp - 273.15;
-    };
+    }
     else if (tempUnit == 'fahrenheit'&&  tempUnitWanted == 'kelvin'){
         newTemp = (temp − 32) * 5/9 + 273.15;
-    };
+    }
     else if (tempUnit == 'kelvin'&&  tempUnitWanted == 'fahrenheit'){
-        newTemp = (temp − 273.15) * 9/5 + 32;
-    };
+        newTemp = (temp − 273.15) 9/5 + 32;
+    }
     return newTemp;
 }
