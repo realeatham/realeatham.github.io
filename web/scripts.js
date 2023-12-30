a3498cac65497068adab99c556454cf85d6b7e2a
realeatham <136866675+realeatham@users.noreply.github.com>
2023-12-30 22:29:25 +1100

    temp 7

--- web/scripts.js
+++ web/scripts.js
@@ -19,20 +19,23 @@ function tempConvert(){
     const temp = document.getElementById('temp');
     let newTemp;
     //i know this can be done with switch. i cannot work it out, if you know how to actually use it please help
-    if (tempUnit == 'fahrenheit'; tempUnitWanted == 'celsius'){
+    if (tempUnit == 'fahrenheit'&&  tempUnitWanted == 'celsius'){
         newTemp = (5/9) * (temp-32);
     };
-    else if (tempUnit == 'celsius'; tempUnitWanted == 'fahrenheit'){
+    else if (tempUnit == 'celsius'&&  tempUnitWanted == 'fahrenheit'){
         newTemp = (temp * 9/5) + 32;
     };
-    else if (tempUnit == 'celsius'; tempUnitWanted == 'kelvin'){
+    else if (tempUnit == 'celsius'&&  tempUnitWanted == 'kelvin'){
         newTemp = temp + 273.15;
     };
-    else if (tempUnit == 'kelvin'; tempUnitWanted == 'celsius'){
+    else if (tempUnit == 'kelvin'&&  tempUnitWanted == 'celsius'){
         newTemp = temp - 273.15;
     };
-    else if (tempUnit == 'fahrenheit'; tempUnitWanted == 'kelvin'){
+    else if (tempUnit == 'fahrenheit'&&  tempUnitWanted == 'kelvin'){
         newTemp = (temp − 32) * 5/9 + 273.15;
+    };
+    else if (tempUnit == 'kelvin'&&  tempUnitWanted == 'fahrenheit'){
+        newTemp = (temp − 273.15) * 9/5 + 32;
     };
     return newTemp;
 }
