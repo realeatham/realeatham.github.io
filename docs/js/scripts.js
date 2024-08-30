4ff886e0e5098f94ba8432082186447330525eef
Ethan <136866675+realeatham@users.noreply.github.com>
2024-08-30 16:45:27 +1000

    librejs

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,3 +1,5 @@
+// @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
+
 // page theme functions
 
 function dark() {
@@ -48,3 +50,5 @@ function tempConvert() {
     }
     return newTemp;
 };
+
+// @license-end
